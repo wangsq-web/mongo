e982b8461e381f39733146f0ac910119bfcbd215
wsq <1475088311@qq.com>
2020-03-30 22:44:03 +0800

    查询数据库并分页

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ var db = require('./model/main.js');
 
 var i = 0;
 
+app.set("view engine","ejs");
+app.use(express.static("./public"));
+
+app.get("/",function(req,res){
+  var page = parseInt(req.query.page) || 0;
+  var limit = parseInt(req.query.limit) || 10;
+
+  var dbname = 'test';
+  db.find(dbname,"teacher",{},{"pageamount": limit,"page": page},function(err,result){
+    if(err){
+      console.log(err)
+      return;
+    }
+    console.log(result)
+    // res.json(result);
+    res.render("index",{
+      "json": result
+    })
+  })
+})
+
 app.get("/add",function(req,res){
   var dbname = 'test';
   db.insertOne(dbname,"teacher",{"name":"小明","count": i++,"age": parseInt(Math.random()*100)},function(err,result){
@@ -15,19 +36,19 @@ app.get("/add",function(req,res){
   })
 })
 
-app.get("/cha",function(req,res){
-  var page = parseInt(req.query.page) || 0;
-  var limit = parseInt(req.query.limit) || 10;
+// app.get("/cha",function(req,res){
+//   var page = parseInt(req.query.page) || 0;
+//   var limit = parseInt(req.query.limit) || 10;
 
-  var dbname = 'test';
-  db.find(dbname,"teacher",{},{"pageamount": limit,"page": page},function(err,result){
-    if(err){
-      console.log(err)
-      return;
-    }
-    console.log(result)
-    res.json(result);
-  })
-})
+//   var dbname = 'test';
+//   db.find(dbname,"teacher",{},{"pageamount": limit,"page": page},function(err,result){
+//     if(err){
+//       console.log(err)
+//       return;
+//     }
+//     console.log(result)
+//     res.json(result);
+//   })
+// })
 
 app.listen(3000);
